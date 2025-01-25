b02486d03a04ec024997d94ae6366f7c72ef0249
Abdullah3245 <abdullahgoher3245@gmail.com>
2025-01-25 10:23:09 -0500

    deployed site

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import Login from './Components/login.js';
 import SignUp from './Components/signup.js';
 import Main from './Components/main.js';
 import Home from './Components/home.js';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={<Home />} /> {/* Default route */}
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
